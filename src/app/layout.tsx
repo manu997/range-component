7bfa31e393d9b4e34a4b107de0c6edd309debb74
manu997 <manuelmorenoasiain@gmail.com>
2024-09-27 00:04:11 +0200

    FontAwesomeIcon main config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,8 @@
-import styles from './globals.module.scss';
+import styles from './layout.module.scss';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+
+config.autoAddCss = false;
 
 export default function RootLayout({
   children,
